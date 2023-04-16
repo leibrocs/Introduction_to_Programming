cb54c552aceb63d91a308453cc30f714b5a35779
leibrocs <116877154+leibrocs@users.noreply.github.com>
2023-04-16 07:34:50 +0200

    Update SentinelDataDownload.R

--- SentinelDataDownload.R
+++ SentinelDataDownload.R
@@ -10,14 +10,11 @@ getwd()
 # set working directory if necessary
 # setwd("path/to/your/wd")
 
+
+
+########## Preparations for downloading the Data ##########
 # define output directory
 dir_out <- "SentinelData/"
-
-
-########## Download Data from Sentinel Copernicus Open Access Hub ##########
-
-# login to Copernicus Open Access Hub
-login_CopHub("your_username")
 
 # AOI (here, an area in northeastern Paraguay) and define it as search area for the Sentinel-2 scenes
 aoi_matrix <- cbind(c(-60.9216, -60.5427, -60.5427, -60.9216), c(-20.7873, -20.7873, -21.1547, -21.1547))
@@ -28,6 +25,13 @@ view_aoi()
 # remark: the data for October 2019 in no longer available and provided in the folder 'ClassificationData/SentinelData' 
 # time_range2019 <- c("2019-10-01", "2019-10-31") 
 time_range2022 <- c("2022-12-01", "2022-12-20")
+
+
+
+########## Download Sentinel-2 Data from ESA Copernicus Open Access Hub ##########
+
+# login to Copernicus Open Access Hub
+login_CopHub("your_username")
 
 # search for images per time range
 records_2022 <- get_records(time_range2022, products = "Sentinel-2")
@@ -64,11 +68,11 @@ plot_previews(previews_2022)
 # download the Sentinel-2 data and save them
 records_2022 <- get_data(records_2022, dir_out = dir_out)
 
-# unzip the downloaded files
+# define path of the .zip-files
 path_2022 <- paste0(getwd(), "/SentinelData/sentinel-2", sep = "")
 zipfile <- list.files(path_2022, pattern = ".zip", full.names = T)
 files
 
 # unzip the downloaded files (might not work for all R versions)
-# if unzipping files does not work, please do so manually
+# if unzipping files does not work, please do it manually
 unzip(files, exdir = path_2022, overwrite = T)
